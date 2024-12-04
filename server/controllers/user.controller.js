56d46806b19f422b0623b42be6213e159fe6779a
Saraha <sah110_@outlook.com>
2024-12-04 14:29:47 -0500

    Create user.controller.js

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,52 @@
+import bcrypt from 'bcryptjs'
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req, res) => {
+    const { username } = req.params;
+
+    try {
+        const user = await User.findOne({ username }).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { fullName, email, username, currentPassword, newPassword } = req.body;
+
+    const userId = req.user._id;
+
+    try {
+        let user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+            return res.status(400).json({ error: "Please provide both current password and new password" });
+        }
+
+        if (currentPassword && newPassword) {
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if (!isMatch) return res.status(400).json({ error: "Current password is incorrect" });
+            if (newPassword.length < 6) {
+                return res.status(400).json({ error: "Password must be at least 6 characters long" });
+            }
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+        }
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+
+        user.password = await user.save();
+
+        user.password = null;
+        return res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
