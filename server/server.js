55dc0be6571ca49f449df68a9415c212f7a89f30
Saraha <sah110_@outlook.com>
2024-12-04 14:29:57 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-
-import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 import assignmentRoutes from "./routes/assignment.route.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -15,7 +18,12 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use("/api/assignments", assignmentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/dist")));
